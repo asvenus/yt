82be285843eaaff01dbb9e43474d11299ed71c14
asvenus <chuyenvu90@gmail.com>
2017-09-30 13:02:59 +0700

    Update account.rb

--- lib/yt/models/account.rb
+++ lib/yt/models/account.rb
@@ -212,11 +212,13 @@ module Yt
           snippet = params.slice :title, :description, :tags, :category_id
           snippet[:categoryId] = snippet.delete(:category_id) if snippet[:category_id]
           body[:snippet] = snippet if snippet.any?
-          body[:status] = {}
+          #body[:status] = {}
           status = params[:privacy_status]
           publish_at = params[:publish_at]
-          body[:status].merge!({privacyStatus: status}) if status
-          body[:status].merge!({publishAt: publish_at}) if publish_at
+          body[:status] = ({privacyStatus: status, publishAt: publish_at}) if status
+          binding.
+          #body[:status].merge!({privacyStatus: status}) if status
+          #body[:status].merge!({publishAt: publish_at}) if publish_at
         end
       end
 
